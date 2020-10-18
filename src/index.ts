3b13bd5211565f821a71a32868b9907eca791b87
SirCremefresh <donato.wolfisberg@gmail.com>
2020-10-18 21:17:49 +0200

    started implementing parsing project files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-// import {Octokit} from '@octokit/rest';
+import {Octokit} from '@octokit/rest';
+import {OctokitResponse, PullsListResponseData, ReposGetResponseData} from '@octokit/types';
 import {program} from 'commander';
-// import {OctokitResponse, ReposGetResponseData} from '@octokit/types';
+import {readdirSync, readFile} from 'fs';
+import yaml from 'js-yaml';
+import path from 'path';
 
 require('dotenv').config();
 
@@ -14,6 +17,12 @@ interface DockerHubWebhookData {
 	owner: string,
 }
 
+interface Project {
+	name: string,
+	images: {
+		[key: string]: { name: string, tag: string }
+	}
+}
 
 function parseInput(): DockerHubWebhookData {
 	program
@@ -43,43 +52,104 @@ const dockerHubWebhookData = parseInput();
 
 console.log(dockerHubWebhookData);
 
-// type ListUserReposResponse = Endpoints['GET /repos/:owner/:repo']['response'];
-//
-// const DEBUG = process.env.DEBUG === 'true';
-// if (DEBUG) {
-// 	console.debug('Debug logging is enabled');
-// }
-//
-// const GITHUB_AUTH_TOKEN = process.env.GITHUB_AUTH_TOKEN;
-// if (GITHUB_AUTH_TOKEN === undefined) {
-// 	console.error('The environment variable: "GITHUB_AUTH_TOKEN" is not set. Killing Application ');
-// 	process.exit(1);
-// } else {
-// 	console.info('Github Auth Token successfully loaded from environment variable. variable: "GITHUB_AUTH_TOKEN"');
-// }
-//
-// // const octokit = new Octokit({
-// // 	auth: GITHUB_AUTH_TOKEN
-// // });
-// //
-//
-//
-// (async () => {
-//
-// 	console.log(process.env.DATA);
-//
-//
-// 	function getDataFromArguments(): any {
-// 		for (const argument of process.argv) {
-// 			return argument;
-// 		}
-// 	}
-//
-// 	// const {
-// 	// 	data
-// 	// } = await octokit.repos.get({
-// 	// 	owner: 'SirCremefresh',
-// 	// 	repo: 'bmw12-cluster',
-// 	// }) as OctokitResponse<ReposGetResponseData>;
-// 	// console.log(data);
-// })();
+
+const DEBUG = process.env.DEBUG === 'true';
+if (DEBUG) {
+	console.debug('Debug logging is enabled');
+}
+
+const GITHUB_AUTH_TOKEN = process.env.GITHUB_AUTH_TOKEN;
+if (GITHUB_AUTH_TOKEN === undefined) {
+	console.error('The environment variable: "GITHUB_AUTH_TOKEN" is not set. Killing Application ');
+	process.exit(1);
+} else {
+	console.info('Github Auth Token successfully loaded from environment variable. variable: "GITHUB_AUTH_TOKEN"');
+}
+
+const octokit = new Octokit({
+	auth: GITHUB_AUTH_TOKEN
+});
+
+
+// octokit.pulls.list({
+// 	owner,
+// 	repo,
+// });
+
+async function getOpenPullRequests() {
+	const {data} = await octokit.pulls.list({
+		owner: 'SirCremefresh',
+		repo: 'bmw12-cluster',
+	}) as OctokitResponse<PullsListResponseData>;
+	return data;
+}
+
+async function readAllProjectConfigurations(): Promise<Project[]> {
+	const workspaceLocation = './workspace/bmw12-cluster/apps';
+	const dirNames = readdirSync(workspaceLocation, {withFileTypes: true})
+		.filter(dir => dir.isDirectory())
+		.map(dir => dir.name);
+
+	return await Promise.all(dirNames.map(dirName => {
+		return loadProjectFromFile(path.join(workspaceLocation, dirName, 'bmw12-application.yaml'));
+	}));
+}
+
+async function loadProjectFromFile(dirName: string): Promise<Project> {
+	const data = await readUtf8File(dirName);
+	return yaml.safeLoad(data) as Project;
+}
+
+function readUtf8File(file: string): Promise<string> {
+	return new Promise((resolve, reject) => {
+		readFile(file, {encoding: 'utf8'}, (err, data) => {
+			if (err)
+				reject(err);
+			resolve(data);
+		});
+	});
+}
+
+function filterProjectsWithImage(projects: Project[], imageName: string) {
+	return projects.filter(project => {
+		return Object.values(project.images).find(image => image.name === imageName) !== undefined;
+	});
+}
+
+(async () => {
+	try {
+		const projects = await readAllProjectConfigurations();
+		// console.log(JSON.stringify(projects));
+		const projectsWithChangedImage = filterProjectsWithImage(projects, dockerHubWebhookData.imageName);
+		console.log(projectsWithChangedImage)
+		// const pullRequests = await getOpenPullRequests();
+		// // const pullRequest = pullRequests.find(pullRequest => pullRequest.head.ref === dockerHubWebhookData)
+		//
+		//
+		// console.log(dockerHubWebhookData);
+		// console.log(pullRequests[0].head.ref);
+		// console.log(pullRequests);
+
+		// await octokit.pulls.create({
+		// 	owner: 'SirCremefresh',
+		// 	repo: 'bmw12-cluster',
+		// 	title: 'Update sample-application',
+		// 	head: 'update/sample-application',
+		// 	base: 'master',
+		// });
+	} catch (e) {
+		console.log(e);
+	}
+
+
+})();
+
+async function test1() {
+	const {
+		data
+	} = await octokit.repos.get({
+		owner: 'SirCremefresh',
+		repo: 'bmw12-cluster',
+	}) as OctokitResponse<ReposGetResponseData>;
+	console.log(data);
+}
