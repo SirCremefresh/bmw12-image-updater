29688ffa6bbd61eec24adca1a0574a07501ee21e
SirCremefresh <donato.wolfisberg@gmail.com>
2020-11-07 22:44:17 +0100

    added write back

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {program} from 'commander';
-import {readdirSync, readFile} from 'fs';
+import {readdirSync, readFile, writeFile} from 'fs';
 import yaml from 'js-yaml';
 import path from 'path';
 import simpleGit, {SimpleGit, SimpleGitOptions} from 'simple-git';
@@ -18,7 +18,13 @@ interface DockerHubWebhookData {
 }
 
 interface Project {
+  fileName: string,
+  projectData: ProjectData
+}
+
+interface ProjectData {
   name: string,
+  dirName: string,
   images: {
     [key: string]: { name: string, tag: string }
   }
@@ -66,6 +72,8 @@ const options: SimpleGitOptions = {
 };
 
 const git: SimpleGit = simpleGit(options);
+const CONFIG_FILE_NAME = 'bmw12-application.yaml';
+
 
 async function readAllProjectConfigurations(): Promise<Project[]> {
   const workspaceLocation = `${dockerHubWebhookData.workspacePath}/apps`;
@@ -74,13 +82,17 @@ async function readAllProjectConfigurations(): Promise<Project[]> {
     .map(dir => dir.name);
 
   return await Promise.all(dirNames.map(dirName => {
-    return loadProjectFromFile(path.join(workspaceLocation, dirName, 'bmw12-application.yaml'));
+    return loadProjectFromFile(path.join(workspaceLocation, dirName, CONFIG_FILE_NAME));
   }));
 }
 
-async function loadProjectFromFile(dirName: string): Promise<Project> {
-  const data = await readUtf8File(dirName);
-  return yaml.safeLoad(data) as Project;
+async function loadProjectFromFile(fileName: string): Promise<Project> {
+  const data = await readUtf8File(fileName);
+  return {fileName: fileName, projectData: yaml.safeLoad(data) as ProjectData};
+}
+
+async function writeProjectToFile(project: Project): Promise<void> {
+  await writeUtf8File(project.fileName, yaml.safeDump(project.projectData));
 }
 
 function readUtf8File(file: string): Promise<string> {
@@ -93,18 +105,28 @@ function readUtf8File(file: string): Promise<string> {
   });
 }
 
+function writeUtf8File(file: string, data: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    writeFile(file, data, {encoding: 'utf8'}, (err) => {
+      if (err)
+        reject(err);
+      resolve();
+    });
+  });
+}
+
 function filterProjectsWithImage(projects: Project[], imageName: string) {
   return projects.filter(project => {
-    return Object.values(project.images).find(image => image.name === imageName) !== undefined;
+    return Object.values(project.projectData.images).find(image => image.name === imageName) !== undefined;
   });
 }
 
 function updateImageInProject(project: Project, imageName: string, imageTag: string): Project {
-  for (let [key, image] of Object.entries(project.images)) {
+  for (let [key, image] of Object.entries(project.projectData.images)) {
     if (image.name !== imageName) {
       continue;
     }
-    project.images[key].tag = imageTag;
+    project.projectData.images[key].tag = imageTag;
   }
   return project;
 }
@@ -115,9 +137,10 @@ function updateImageInProject(project: Project, imageName: string, imageTag: str
     const projectsWithChangedImage = filterProjectsWithImage(projects, dockerHubWebhookData.imageName);
 
     for (const project of projectsWithChangedImage) {
-      console.log(`Updating image in Project: ${project.name}`);
+      console.log(`Updating image in Project: ${project.projectData.name}`);
       const updateProject = updateImageInProject(project, dockerHubWebhookData.imageName, dockerHubWebhookData.tag);
 
+      await writeProjectToFile(updateProject)
 
       console.log(updateProject);
     }
