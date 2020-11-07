9f9ded46f0dc5dc8ef18c3dd8878a8cb302a4125
SirCremefresh <donato.wolfisberg@gmail.com>
2020-11-07 20:49:04 +0100

    removed requirement of auth token

--- src/index.ts
+++ src/index.ts
@@ -55,14 +55,6 @@ if (DEBUG) {
   console.debug('Debug logging is enabled');
 }
 
-const GITHUB_AUTH_TOKEN = process.env.GITHUB_AUTH_TOKEN;
-if (GITHUB_AUTH_TOKEN === undefined) {
-  console.error('The environment variable: "GITHUB_AUTH_TOKEN" is not set. Killing Application ');
-  process.exit(1);
-} else {
-  console.info('Github Auth Token successfully loaded from environment variable. variable: "GITHUB_AUTH_TOKEN"');
-}
-
 const options: SimpleGitOptions = {
   baseDir: GIT_PROJECT_BASE,
   binary: 'git',
